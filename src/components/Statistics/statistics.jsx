24088ee39eeea9233020554a1adb06cc3d51eb83
Kostyantin-spec <134406433+Kostyantin-spec@users.noreply.github.com>
2024-01-06 20:47:33 +0200

    and

---
+++ src/components/Statistics/statistics.jsx
@@ -0,0 +1,21 @@
+import css from './statistics.module.css';
+
+export const Statistics = ({
+    good,
+    bad,
+    neutral,
+    total,
+    positivePercentage,
+}) => {
+    return (
+        <ul className={css.Statistics_list}>
+            <li className={css.Statistics_item}>Good: {good}</li>
+            <li className={css.Statistics_item}>Neutral: {neutral}</li>
+            <li className={css.Statistics_item}>Bad: {bad}</li>
+            <li className={css.Statistics_item}>Total: {total}</li>
+            <li className={css.Statistics_item}>
+                Positive Feedback: {positivePercentage}%
+            </li>
+        </ul>
+    );
+};
